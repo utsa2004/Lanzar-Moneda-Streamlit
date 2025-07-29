808518e5b94f510be88dd3e84307506985af4467
Oscar Aranda <utsa845@gmail.com>
2025-07-29 00:04:56 -0600

    version 1

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
+import pandas as pd
+import scipy.stats
 import streamlit as st
-import random
+import time
 
-st.title("Lanzar una moneda")
-st.write("Haz clic en el botón para lanzar la moneda.")
+# Variables de estado de la sesión
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
 
-if st.button("Lanzar"):
-    resultado = random.choice(["Cara", "Cruz"])
-    st.success(f"¡Salió {resultado}!")
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+
+st.header('Lanzar una moneda')
+
+# Gráfico inicial
+chart = st.line_chart([0.5])
+
+# Función que simula los lanzamientos
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+# Elementos de la interfaz
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+# Lógica principal
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]],
+                     columns=['no', 'iteraciones', 'media'])
+    ], axis=0).reset_index(drop=True)
+
+# Mostrar tabla de resultados
+st.write(st.session_state['df_experiment_results'])
+
 
